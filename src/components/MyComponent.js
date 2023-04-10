76c0bfe4636d5064e2a6b21449331edd92c8b4f3
User Name <username@gmail.com>
2023-04-10 15:50:30 +0700

    bai2

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  // jsx
+  render() {
+    return (
+      <div>
+        {" "}
+        duy dep trai
+        {Math.random()}
+      </div>
+    );
+  }
+}
+export default MyComponent;

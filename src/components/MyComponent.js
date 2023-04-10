62834c2c661d062ba8a7b310fb6abbf9b10bfcbc
User Name <username@gmail.com>
2023-04-10 16:46:08 +0700

    bai3

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "duy",
+    address: "duy dep trai",
+    age: 26,
+  };
+  handleClick() {
+    console.log("duy dep trai");
+  }
+  handleOnmoverOver(even) {
+    console.log(even);
+  }
   // jsx
   render() {
     return (
       <div>
-        {" "}
-        duy dep trai
-        {Math.random()}
+        my name id {this.state.name} and i fron {this.state.address}
+        <button onMouseOver={this.handleOnmoverOver}>click me`</button>
+        <button onClick={this.handleClick}>click me`</button>
       </div>
     );
   }

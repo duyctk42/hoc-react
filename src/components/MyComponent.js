296106b94a1735c0019c8de8f36648791501a759
User Name <username@gmail.com>
2023-04-10 17:15:20 +0700

    bai 4

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,19 +6,29 @@ class MyComponent extends React.Component {
     address: "duy dep trai",
     age: 26,
   };
-  handleClick() {
-    console.log("duy dep trai");
-  }
+  handleClick = (event) => {
+    console.log("my name is", this.state.age);
+    this.setState({
+      name: "kataris",
+      age: Math.floor(Math.random() * 10),
+    });
+  };
   handleOnmoverOver(even) {
-    console.log(even);
+    console.log("alo");
   }
   // jsx
   render() {
     return (
       <div>
-        my name id {this.state.name} and i fron {this.state.address}
+        my name id {this.state.name} and i fron {this.state.age}
         <button onMouseOver={this.handleOnmoverOver}>click me`</button>
-        <button onClick={this.handleClick}>click me`</button>
+        <button
+          onClick={(event) => {
+            this.handleClick();
+          }}
+        >
+          click me`
+        </button>
       </div>
     );
   }

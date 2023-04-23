103f47ede2aead2e69a492afc107ef827cfc46d1
User Name <username@gmail.com>
2023-04-23 15:41:15 +0700

    làm thanh điều hướng trang

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <div> test hello</div>
+      <div>
+        <button>
+          <Link to="/User">user</Link>
+        </button>
+        <button>
+          <Link to="/Admin">Admin</Link>
+        </button>
+      </div>
     </div>
   );
 };
